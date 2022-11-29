e9f60a3fff0e07823e10098e063e7d57a38497bf
Nikita Veretenov <nik_loko@mail.ru>
2022-11-29 15:26:01 +0300

    1 edit

---
+++ src/Second.java
@@ -0,0 +1,18 @@
+
+public class Second {
+    public static void main(String[] args) {
+        System.out.println(fraction(4.3));
+
+    }
+
+    public static double fraction(double x){
+       int res=(int)x;
+        System.out.println(res);
+       return res;
+
+
+    }
+
+
+}
+
